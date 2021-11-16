29e7f32500f5d1dedc5a28644e90279f19cf881f
Samuel Oreste Abreu <s281568@studenti.polito.it>
2021-11-16 11:07:21 +0100

    Feat: Added minmax implementation

---
+++ sol.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 11:38:02 2021
+
+@author: foxtrot
+"""
+
+import utils.squilibs as c4
+import utils.tree as t
+import random
+import numpy as np
+
+PLAYER_1 = 1
+PLAYER_2 = -1
+
+BRANCHING_FACTOR = 2
+MAX_DEPTH = 15
+
+
+def dummy_plays(board, turns):
+    for _ in range(turns):
+        place_randomly(board, PLAYER_1)
+        place_randomly(board, PLAYER_2)
+    print(board)
+
+
+def place_randomly(board, player):
+    play = random.sample(c4.valid_moves(board), 1)[0]
+    c4.play(board, play, player)
+
+
+def mcst(root, player):
+    best_node = None
+    for _ in range(100):
+        best_node = t.select_best(root)
+        t.expand(best_node, player)
+        won = t.simulate(best_node, player)
+        best_node.rewards += won
+        player = -player
+
+    print(best_node.state)
+
+    ptr = best_node.parent
+
+    while ptr.player != 0:
+        print(best_node.state)
+
+
+def _minmax(board, player, level):
+
+    if level == MAX_DEPTH or not c4.valid_moves(board):
+        return 0
+
+    if c4.four_in_a_row(board, 1):
+        return 1
+    elif c4.four_in_a_row(board, -1):
+        return -1
+
+    board_moves = np.array(c4.valid_moves(board))
+
+    val = -2**32 if player == PLAYER_1 else 2**32
+
+    for i in range(BRANCHING_FACTOR):
+
+        board_copy = board.copy()
+        if len(board_moves) == 0:
+            break
+        move = np.random.choice(board_moves, 1)[0]
+        board_moves = board_moves[board_moves != move]
+        c4.play(board_copy, move, player)
+        sol = _minmax(board_copy, -player, level + 1)
+        val = max(val, sol) if player == PLAYER_1 else min(val, sol)
+
+    return val
+
+
+board = np.zeros((c4.NUM_COLUMNS, c4.COLUMN_HEIGHT), dtype=np.byte)
+
+c4.play(board, 3, 1)
+c4.play(board, 0, -1)
+c4.play(board, 4, 1)
+c4.play(board, 0, -1)
+c4.play(board, 5, 1)
+
+print(_minmax(board, PLAYER_1, 0))
+
+# root = t.Node(board, 0)
+
+# mcst(root, PLAYER_1)
---
+++ utils/squilibs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright **`(c)`** 2021 Giovanni Squillero `<squillero@polito.it>`  
+[`https://github.com/squillero/computational-intelligence`](https://github.com/squillero/computational-intelligence)  
+Free for personal or classroom use; see 'LICENCE.md' for details.
+"""
+
+import numpy as np
+
+NUM_COLUMNS = 7
+COLUMN_HEIGHT = 6
+FOUR = 4
+
+# Board can be initiatilized with `board = np.zeros((NUM_COLUMNS, COLUMN_HEIGHT), dtype=np.byte)`
+# Notez Bien: Connect 4 "columns" are actually NumPy "rows"
+
+
+def valid_moves(board):
+    """Returns columns where a disc may be played"""
+    return [n for n in range(NUM_COLUMNS) if board[n, COLUMN_HEIGHT - 1] == 0]
+
+
+def play(board, column, player):
+    """Updates `board` as `player` drops a disc in `column`"""
+    (index,) = next((i for i, v in np.ndenumerate(board[column]) if v == 0))
+    board[column, index] = player
+
+
+def take_back(board, column):
+    """Updates `board` removing top disc from `column`"""
+    (index,) = [i for i, v in np.ndenumerate(board[column]) if v != 0][-1]
+    board[column, index] = 0
+
+
+def four_in_a_row(board, player):
+    """Checks if `player` has a 4-piece line"""
+    return (
+        any(
+            all(board[c, r] == player)
+            for c in range(NUM_COLUMNS)
+            for r in (list(range(n, n + FOUR)) for n in range(COLUMN_HEIGHT - FOUR + 1))
+        )
+        or any(
+            all(board[c, r] == player)
+            for r in range(COLUMN_HEIGHT)
+            for c in (list(range(n, n + FOUR)) for n in range(NUM_COLUMNS - FOUR + 1))
+        )
+        or any(
+            np.all(board[diag] == player)
+            for diag in (
+                (range(ro, ro + FOUR), range(co, co + FOUR))
+                for ro in range(0, NUM_COLUMNS - FOUR + 1)
+                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
+            )
+        )
+        or any(
+            np.all(board[diag] == player)
+            for diag in (
+                (range(ro, ro + FOUR), range(co + FOUR - 1, co - 1, -1))
+                for ro in range(0, NUM_COLUMNS - FOUR + 1)
+                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
+            )
+        )
+    )
